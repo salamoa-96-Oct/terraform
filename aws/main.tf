9a44472bd64601bf8d87c9da09758344d0266815
salamoa1212 <mjsmjh1212@naver.com>
2022-02-22 16:01:22 +0900

    aws-terraform

--- aws/main.tf
+++ aws/main.tf
@@ -104,3 +104,12 @@ resource "Security" "mjs_securitygroup" {
   }
 }
 
+#################### Subnet ###################
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "pub-subnet-mjs"
+  }
+}
