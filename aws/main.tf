649c2a56a3670284e8a9dd1a4c8f5eb49901c51c
salamoa1212 <mjsmjh1212@naver.com>
2022-02-22 15:35:16 +0900

    aws-terraform

--- aws/main.tf
+++ aws/main.tf
@@ -16,3 +16,91 @@ provider "aws" {
 
 ################### VPC ########################
 
+resource "aws_vpc" "mjs-vpc" {
+  cidr_block           = var.aws_vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Owner = "${var.owner}"
+    Name = "mjs_vpc"
+    Service = "k8s_mjs"
+  }
+}
+
+################## IGW ######################
+resource "aws_internet_gateway" "mjs-igw" {
+  vpc_id = aws_vpc.mjs-vpc.id
+
+  tags = {
+    Owner = mjs
+    Name = "mjs_igw"
+    Service = "k8s_mjs"
+  }
+}
+
+################# nat gateway ################
+
+resource "aws_nat_gateway" "mjs-nat" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.private_2.id
+}
+
+################# Routing table #################
+
+resource "aws_route_table" "mjs_public_rt" {
+  vpc_id = aws_vpc.mjs-vpc.id
+
+  route {
+    cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.mjs-igw.id
+  }
+
+  tags = {
+    Owner = mjs
+    Name = "mjs_public_rt"
+    Service = "k8s_mjs"
+  }
+}
+
+resource "aws_route_table" "mjs_pri_rt" {
+  vpc_id = aws_vpc.mjs-vpc.id
+
+  route {
+    cidr_block = "::/0"
+    gateway_id = aws_nat_gateway.mjs-nat.id
+  }
+
+  tags = {
+    Owner = mjs
+    Name = "mjs_pri_rt"
+    Service = "k8s_mjs"
+  }
+}
+
+################## Security Group ######################
+resource "Security" "mjs_securitygroup" {
+  name = "mjs_securitygroup"
+  description = "Test_mjs"
+  vpc_id = aws_vpc.mjs-vpc.id
+  
+   ingress {
+    description      = "all Access"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_Access"
+  }
+}
+
