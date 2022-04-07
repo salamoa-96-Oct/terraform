8f5846a9293c3afdd441794dfdf9d84304abdb47
salamoa1212 <mjsmjh1212@naver.com>
2022-04-07 11:40:39 +0900

    azure-terraform

--- azure/main.tf
+++ azure/main.tf
@@ -12,7 +12,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "mjs-test-RG" {
-  name     = "mjs-test-RG"
-  location = "West Europe"
+  name      = "kuberixtestRG"
+  location  = var.resource_group_location
 }
 
+resource "azurerm_virtual_network" "mjs-virtual-network" {
+  name                = "mjs-virtual-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
